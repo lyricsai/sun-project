1585acc82d20b1b51e201846c046f2dea82cadf8
Anastasiya Litvinenka <loffy@rambler.ru>
2021-08-26 23:27:21 +0300

    added Header, Nav, Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__logo">Anastasiya Litvinenka</div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <ul>
+                <li><a href="google.com" class="">Profile</a></li>
+                <li><a href="google.com" class="">News</a></li>
+                <li><a href="google.com" class="">Messages</a></li>
+                <li><a href="google.com" class="">Music</a></li>
+                <li><a href="google.com" class="">Sun Project</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
